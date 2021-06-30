a268a2620493c120c534331b86a603bb8fb0b993
2020147551 <2019147551@yonsei.ac.kr>
2021-06-30 23:48:45 +0900

    .

--- main.py
+++ main.py
@@ -71,3 +71,13 @@ import sys
 #
 # print(y, x)
 
+# 구현 알고리즘 예제 4-2번
+# num = int(sys.stdin.readline().rstrip())
+# count = 0
+# for i in range(num+1):
+#     for j in range(60):
+#         for k in range(60):
+#             if "3" in str(i) + str(j) + str(k):
+#                 count+=1
+#
+# print(count)
