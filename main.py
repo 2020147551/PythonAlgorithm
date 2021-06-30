203e022040481c5176d618bca3a9f07d93e87b2a
2020147551 <2019147551@yonsei.ac.kr>
2021-06-30 12:45:30 +0900

    first

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+
+#그리디 알고리즘 1번
+#
+# a, b, c = map(int, sys.stdin.readline().rstrip().split())
+# num_list = list(map(int, sys.stdin.readline().rstrip().split()))
+# count = 1
+# total = 0
+# sorted_list = sorted(num_list)
+#
+# for i in range(b):
+#     if count % c == 0:
+#         total += sorted_list[-2]
+#     else:
+#         total += sorted_list[-1]
+#     count += 1
+#
+# print(total)
+
+#그리디 알고리즘 2번
+#
+# a, b = map(int, sys.stdin.readline().rstrip().split())
+# num_list = list()
+# for i in range(a):
+#     num_list.append(min(list(map(int, sys.stdin.readline().rstrip().split()))))
+#
+# print(max(num_list))
+
+#그리디 알고리즘 3번
+
