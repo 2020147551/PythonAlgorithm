91e7ad81563dd50f9a0c5b2ee81b7ddc3c52ce97
Bokdol11859 <2019147551@yonsei.ac.kr>
2021-08-21 17:20:54 +0900

    ICPC 신촌 캠프 대회

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-#그리디 알고리즘 1번 큰수의 법칙
+# 그리디 알고리즘 1번 큰수의 법칙
 #
 # a, b, c = map(int, sys.stdin.readline().rstrip().split())
 # num_list = list(map(int, sys.stdin.readline().rstrip().split()))
@@ -17,7 +17,7 @@ import sys
 #
 # print(total)
 
-#그리디 알고리즘 2번 숫자 카드 게임
+# 그리디 알고리즘 2번 숫자 카드 게임
 #
 # a, b = map(int, sys.stdin.readline().rstrip().split())
 # num_list = list()
@@ -26,7 +26,7 @@ import sys
 #
 # print(max(num_list))
 
-#그리디 알고리즘 3번 1이 될 때까지
+# 그리디 알고리즘 3번 1이 될 때까지
 #
 # n, k = map(int, sys.stdin.readline().rstrip().split())
 # count = 0
@@ -43,7 +43,7 @@ import sys
 #
 # print(count)
 
-#그리디 알고리즘 기출문제 1번 모험가 길드
+# 그리디 알고리즘 기출문제 1번 모험가 길드
 #
 # num_of_people = int(sys.stdin.readline().rstrip())
 # list_of_fear = list(map(int, sys.stdin.readline().rstrip().split()))
@@ -60,7 +60,7 @@ import sys
 #
 # print(group)
 
-#그리디 알고리즘 기출문제 2번 곱하기 혹은 더하기
+# 그리디 알고리즘 기출문제 2번 곱하기 혹은 더하기
 #
 # num_list = list(sys.stdin.readline().rstrip())
 # for i in range(len(num_list)-1):
@@ -72,7 +72,7 @@ import sys
 #
 # print(num_list[len(num_list)-1])
 
-#그리디 알고리즘 기출문제 3번 문자열 뒤집기
+# 그리디 알고리즘 기출문제 3번 문자열 뒤집기
 
 # num_list = list(map(int, sys.stdin.readline().rstrip()))
 # count_zero = 0
@@ -90,7 +90,7 @@ import sys
 #
 # print(min(count_zero, count_one))
 
-#그리디 알고리즘 기출문제 4번 만들 수 없는 금액
+# 그리디 알고리즘 기출문제 4번 만들 수 없는 금액
 #############################################################7-4-2021 거의 한시간동안 풀지 못해서 답안 보고 풂##############################################################################
 # num = int(sys.stdin.readline().rstrip())
 #
@@ -105,8 +105,7 @@ import sys
 #
 # print(target)
 
-#그리디 알고리즘 기출문제 5번 볼링공 고르기
-
+# 그리디 알고리즘 기출문제 5번 볼링공 고르기
 
 
 # 구현 알고리즘 예제 4-1번
@@ -148,4 +147,124 @@ import sys
 #
 # print(count)
 
-# 구현 알고리즘 실전 문제 2번 왕실의 나이트
+
+#ICPC 신촌 캠프 대회
+# 1 AC
+# import sys
+# digit = int(sys.stdin.readline().rstrip())
+# binary = int(sys.stdin.readline().rstrip(), 2)
+# division = int(sys.stdin.readline().rstrip())
+# num = pow(2, division)
+# # print(binary)
+# # print(num)
+# if binary % num == 0:
+#     print("YES")
+# else:
+#     print('NO')
+
+
+# 2 AC
+# import sys
+#
+# num_people, card_num = map(int, sys.stdin.readline().rstrip().split())
+# arr = []
+# final = []
+# final1 = []
+# for i in range(num_people):
+#     arr.append(list(map(int, sys.stdin.readline().rstrip().split())))
+#
+# for i in range(num_people):
+#     for j in range(card_num):
+#         final.append(arr[i][j])
+#         final1.append(i+1)
+# index = 0
+# while len(final) > 1:
+#
+#     start = final[index]
+#     final.pop(index)
+#     final1.pop(index)
+#     index += start - 1
+#     index %= len(final)
+#
+# ans = final[0]
+# ans1 = final1[0]
+#
+# print(ans1, ans)
+
+
+# 3 미해결
+# import sys
+#
+# num = int(sys.stdin.readline().rstrip())
+#
+# arr = []
+# for i in range(1, num+1):
+#     arr.append(i)
+#
+# for i in range(num-1, 0, -1):
+#     if arr[i-1] < arr[i]:
+#         for j in range(num-1, 0, -1):
+#             if arr[i-1] < arr[j]:
+#                 arr[i-1], arr[j] = arr[j], arr[i-1]
+#                 arr = arr[:i] + sorted(arr[i:])
+#     check_arr = []
+#     dp = [0] * num
+#     dp[0] = arr[0]
+#     check_arr.append(dp[0] % num)
+#     for i in range(1, num):
+#         dp[i] = dp[i - 1] + arr[i]
+#         check_arr.append(dp[i] % num)
+#         check_arr = list(set(check_arr))
+#         if len(check_arr) >= ((num / 2) + 1): break
+#
+#     if len(check_arr) < ((num / 2) + 1):
+#         print(*arr)
+#         break
+#
+#     continue
+
+#6 미해결
+# import sys
+# from collections import deque
+# height, width = map(int, sys.stdin.readline().rstrip().split())
+# world = []
+# world_moves = []
+# startY = 0
+# startX = 0
+# endY = 0
+# endX = 0
+# for i in range(height):
+#     temp = list(sys.stdin.readline().rstrip())
+#     world.append(temp)
+#     world_moves.append([0]*len(temp))
+#     if 'E' in temp:
+#         endY = i
+#         endX = temp.index('E')
+#     if 'C' in temp:
+#         startY = i
+#         startX = temp.index('C')
+#
+# moveY = (-1, 1, 0, 0)
+# moveX = (0, 0, -1, 1)
+#
+# def bfs(startY, startX):
+#     queue = deque()
+#     queue.append([startY, startX])
+#     world_moves[startY][startX] = 1
+#     while queue:
+#         cur = queue.popleft()
+#         for i in range(4):
+#             tempY = cur[0] + moveY[i]
+#             tempX = cur[1] + moveX[i]
+#             if 0 <= tempY < height and 0 <= tempX < width:
+#                 if world_moves[tempY][tempX] == 0:
+#                     if world[tempY][tempX] != 'D':
+#                         if world[tempY][tempX] == 'L':
+#                             world_moves[tempY][tempX] = world_moves[cur[0]][cur[1]] + 5
+#                             queue.append([tempY, tempX])
+#                         if world[tempY][tempX] == 'X':
+#
+#
+# bfs(startY, startX)
+#
+# print(world)
